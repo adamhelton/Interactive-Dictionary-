8f8943e0b3bc29d989496867e58883ef6dc18ee5
Adam Helton <helton93@protonmail.com>
2019-04-09 09:00:51 -0400

    added title checker

--- interactive_dict.py
+++ interactive_dict.py
@@ -1,26 +1,28 @@
 import json
 from difflib import get_close_matches
 
-data = json.load(open('data.json'))
+data = json.load(open("data.json"))
 
 def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[w.title()]
     elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input('Did you mean %s instead? Enter Y if yes, or N if no: ' % get_close_matches(w, data.keys())[0])
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
             return data[get_close_matches(w, data.keys())[0]]
         elif yn == "N":
             return "The word doesn't exist. Please double check it."
-        else: 
-            return "Your entry wasn't understood, please enter a 'N' or 'Y'"
+        else:
+            return "Your entry wasn't understood."
     else:
         return "The word doesn't exist. Please double check it."
+     
 
-word = input('Enter Word: ')
-
-output = (translate(word))
+word = input("Enter word: ")
+output = translate(word)
 
 if type(output) == list:
     for item in output:
