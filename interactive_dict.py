481b67511f85a09148b1031b25874635053ea68b
Adam Helton <helton93@protonmail.com>
2019-04-08 21:16:23 -0400

    added .lower

--- interactive_dict.py
+++ interactive_dict.py
@@ -3,7 +3,11 @@ import json
 data = json.load(open('data.json'))
 
 def translate(w):
-    return data[w]
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return "The word doesn't exist. Please double check it."
 
 word = input('Enter Word: ')
 
